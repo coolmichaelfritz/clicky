c01ed93bb6c63abe4d58b0f398e7f7e446471959
Michael Fritz and Thomas Doherty <michael@>
2018-02-05 20:18:35 -0800

    Add toggleFade and toggleSlide. Change speeds.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 jQuery(document).ready(function() {
   $(".clickable").click(function() {
-    $("#initially-showing").toggle();
-    $("#initially-hidden").toggle();
+    $("#initially-showing").toggle("fast");
+    $("#initially-hidden").toggle("fast");
+
+  });
+
+  $(".clickable-fade").click(function() {
+    $("#initially-showing-fade").fadeToggle("slow");
+    $("#initially-hidden-fade").fadeToggle("slow");
+
+  });
+
+  $(".clickable-slide").click(function() {
+    $("#initially-showing-slide").slideToggle(3000);
+    $("#initially-hidden-slide").slideToggle(1000);
+
   });
 });
